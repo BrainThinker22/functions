a41ae9a3cc90c8f9c43e21e709405e70c5c2f182
BrainThinker22 <gmpreslavcho@gmail.com>
2024-08-21 14:15:47 +0200

    Add files via upload

---
+++ functions-after-main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void happyBirthday(std::string name, int age);
+
+
+int main()
+{
+	std::string name = "Preslav";
+	int age = 25;
+
+	happyBirthday(name, age);
+
+	return 0;
+}
+
+void happyBirthday(std::string name, int age) {
+	std::cout << "Happy Birthday to " << name << '\n';
+	std::cout << "Happy Birthday to " << name << '\n';
+	std::cout << "Happy Birthday dear " << name << '\n';
+	std::cout << "Happy Birthday to " << name << '\n';
+	std::cout << "You are " << age << " years old!\n";
+}
+
+
+
